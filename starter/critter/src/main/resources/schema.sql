6b81096f280bf85fa0d9998d7eedc417f9ec816e
jzProg <john_z94@hotmail.com>
2021-03-01 20:54:05 +0200

    chore: properties config + sql script added

---
+++ starter/critter/src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS USERS (
+    user_id BIGINT PRIMARY KEY auto_increment,
+    dtype VARCHAR(31),
+    name VARCHAR(50),
+    phone_number VARCHAR(50),
+    notes VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS PET (
+    pet_id BIGINT PRIMARY KEY auto_increment,
+    type VARCHAR(20),
+    name VARCHAR(50),
+    birth_date DATE,
+    notes VARCHAR(100),
+    owner_id BIGINT,
+    foreign key (owner_id) references USERS(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS SCHEDULE (
+    id BIGINT PRIMARY KEY auto_increment,
+    activities VARCHAR(20),
+    date DATE,
+    pet_id BIGINT,
+    user_id BIGINT,
+    foreign key (user_id) references USERS(user_id),
+    foreign key (pet_id) references PET(pet_id)
+);
+
